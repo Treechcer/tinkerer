d08c700b0b99cdadc3f431ff5894aa72f7ddb29b
Treechcer <treechcer@gmail.com>
2025-12-08 18:19:45 +0100

    just added item "JSON" index file

---
+++ source/items/itemIndex.lua
@@ -0,0 +1,11 @@
+--HERE WILL BE ONLY ITEMS AS "JSON" FUNCTIONS FOR ITEMS WILL BE IN OTHER FILES
+
+--This is temporary item, it shows how I want to store items later
+
+items = {
+    ironOre = {
+        maxStackSize = 64,
+        use = "furnace",
+        attack = 1,
+    }
+}
