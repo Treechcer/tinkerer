fa17873b451c3b8ff5a5c3525c2bdc18574ce703
Treechcer <treechcer@gmail.com>
2025-12-09 16:47:56 +0100

    fixed the cursor snapping where it should, to the tiles yk

--- source/game/player.lua
+++ source/game/player.lua
@@ -31,13 +31,13 @@ function player.init() -- initialises the position of player
     player.position.x = math.floor(game.width / 2 - player.size.width / 2)
     player.position.y = math.floor(game.height / 2 - player.size.height / 2)
 
-    player.position.absX, player.position.absY = renderer.calculateTile(renderer.getAbsolutePos(player.position.x, player.position.y))
-    print(player.position.absX, " ", player.position.absY)
+    player.position.absX, player.position.absY = renderer.calculateTile(player.position.x, player.position.y)
+    --print(player.position.absX, " ", player.position.absY)
 end
 
 function player.cursor.updatePos() -- updates mouse position every frame - even calculates the tiles it's on
-    player.cursor.x = love.mouse.getX()
-    player.cursor.y = love.mouse.getY()
+    player.cursor.x = love.mouse.getX() + player.camera.x
+    player.cursor.y = love.mouse.getY() + player.camera.y
 
     player.cursor.tileX, player.cursor.tileY = renderer.calculateTile(player.cursor.x, player.cursor.y)
 end
--- source/graphics/renderer.lua
+++ source/graphics/renderer.lua
@@ -33,8 +33,11 @@ function renderer.gameStateRenderer() -- rendere everything when it's gamestate
     --love.graphics.rectangle("fill", player.cursor.tileX * map.tileSize, player.cursor.tileY * map.tileSize, map.tileSize, map.tileSize)
     renderer.mapRender()
     cursor = spw.sprites.cursor
-    love.graphics.draw(cursor.sprs[cursor.index], player.cursor.tileX * map.tileSize, player.cursor.tileY * map.tileSize,
-    0, map.tileSize / cursor.sprs[cursor.index]:getWidth(), map.tileSize / cursor.sprs[cursor.index]:getHeight())
+
+    local sx, sy = renderer.getAbsolutePos(renderer.getWorldPos(player.cursor.tileX, player.cursor.tileY))
+
+    love.graphics.draw(cursor.sprs[cursor.index], sx, sy,
+        0, map.tileSize / cursor.sprs[cursor.index]:getWidth(), map.tileSize / cursor.sprs[cursor.index]:getHeight())
 
     x, y = renderer.getAbsolutePos(player.position.x, player.position.y)
     love.graphics.rectangle("fill", x, y, player.size.width, player.size.height)
@@ -47,7 +50,14 @@ function renderer.menuStateRenderer() -- render when it's menu time
 
 end
 
-function renderer.getAbsolutePos(x, y) -- this gets the absolute postion relative to camera
+function renderer.getWorldPos(x,y) -- this gets you world position **from tile**
+    wx = x * map.tileSize
+    wy = y * map.tileSize
+
+    return wx, wy
+end
+
+function renderer.getAbsolutePos(x, y) -- this gets the absolute postion relative to camera 
     tempX = x - player.camera.x
     tempY = y - player.camera.y
 
