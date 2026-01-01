27437f7e4675690a5f5e7a60f91ed20780b07fca
treecher <treechcer@gmail.com>
2026-01-01 17:31:40 +0100

    started working on something... Porting it to psp lmao because why not? yes that's why I can't use love.graphisc.getHeigth etc... sadly

--- source/game/game.lua
+++ source/game/game.lua
@@ -15,8 +15,8 @@ game = {
             etc = 1
         },
     },
-    height = love.graphics.getHeight(),
-    width = love.graphics.getWidth(), --this will be possible to change of course, but I have to have some basic info for beginning
+    height = 272,
+    width = 480, --this will be possible to change of course, but I have to have some basic info for beginning
     state = "game" -- this is to know what to render and what behaviour it neeeds to do, if menu, game etc...
 }
 
