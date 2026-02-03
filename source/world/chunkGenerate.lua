a1f7612c857ba3a7630698c7df2440725496d7db
treecher <treechcer@gmail.com>
2026-02-03 20:35:28 +0100

    fixed the generation, now it spawns ALWAYS in grass chunk and now it's correct? I think idk

--- source/world/chunkGenerate.lua
+++ source/world/chunkGenerate.lua
@@ -35,6 +35,19 @@ local function swaperoo()
         tab[index] = chunkGenerate.data.chunkNames[value]
     end
 
+    --grass is ALWAYS the starter biome!
+
+    if tab[1] ~= "grass" then
+        for i = 2, #tab do
+            if tab[i] == "grass" then
+                local temporary = tab[i]
+                tab[i] = tab[1]
+                tab[1] = temporary
+                break
+            end
+        end
+    end
+
     return tab
 end
 
@@ -51,15 +64,13 @@ function chunkGenerate.f.makeChunk(middle, x, y)
     t.chunkData = chunks[math.random(1, chunkGenerate.data.chunkNums)]
     --t.biome = chunkGenerate.data.chunkNames[math.random(1, #chunkGenerate.data.chunkNames)]
     
-    --TODO: fix the 4th being incorrect (bottom corners)
-
     if (x >= 3 and x <= 5) and (y >= 3 and y <= 5) then
         t.biome = chunkGenerate.data.chunkNames[1]
     elseif (x >= 1 and x <= 2) and (y >= 0 and y <= 7) and not (y == 7 and x == 2) and not (y == 1 and x == 2) then
         t.biome = chunkGenerate.data.chunkNames[2]
-    elseif y <= 7 and y >= 5 then
+    elseif y <= 7 and y > 5 and not (x == 7) and not (x == 7 and y == 6) or (x == 7 and y == 7) then
         t.biome = chunkGenerate.data.chunkNames[3]
-    elseif (x >= 6 and x <= 7) and (y >= 0 and y <= 7) and not (y == 7 and x == 6) and not (y == 1 and x == 6) then
+    elseif (x >= 6 and x <= 7) and (y >= 0 and y < 7) and not (y == 1 and x == 6) then
         t.biome = chunkGenerate.data.chunkNames[4]
     else
         t.biome = chunkGenerate.data.chunkNames[5]
