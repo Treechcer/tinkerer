a37deb0b77d2ac089ea3bfd30890dec385464f93
treecher <treechcer@gmail.com>
2026-03-06 08:01:22 +0100

    you can now add items through console

--- source/game/console/console.lua
+++ source/game/console/console.lua
@@ -1,7 +1,7 @@
 --THIS FILE MIGHT BE MOVED? I DIDN'T KNEW WHERE TO PUT IT!!!
 
 console = {
-    render = true,
+    render = false,
     messages = {},
     maxMessages = 5,
     height = 400, --(px)
@@ -22,6 +22,31 @@ console = {
             end
 
             entities.makeNewOne(...)
+        end,
+        give = function (...)
+            if select("#", ...) > 2 then
+                console.commands.print("Too many arguments.")
+                return
+            end
+
+            local args = {...}
+            if itemIndex[args[1]] ~= nil and tonumber(args[2]) > 0 then
+                if tonumber(args[2]) > itemIndex[args[1]].maxStackSize then
+                    --I thought addItem was making something like this? I'll have to look into it later
+                    local c = tonumber(args[2])
+                    while c > 0 do
+                        inventory.functions.addItem(args[1], c)
+                        c = c - itemIndex[args[1]].maxStackSize
+                        if c < 0 then
+                            c = 0
+                        end
+                    end
+                else
+                    inventory.functions.addItem(args[1], tonumber(args[2]))
+                end
+            else
+                console.commands.print("\"" .. args[1] .. "\"" .. " doesn't exist.")
+            end
         end
     }
 }
