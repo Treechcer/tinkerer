549a732b83bff64379b191051c2d4c67aa4d20ae
treecher <treechcer@gmail.com>
2026-03-07 17:31:58 +0100

    technically nothing, just tried copying code from this article "https://www.hackerearth.com/blog/creating-first-2d-game-algorithm" - I'll redo it later and make it actually work, I don't want to work on this anymore - also this is like 3rd rewrite, I didn't commit *that* one

--- source/entities/npc/pathfinding.lua
+++ source/entities/npc/pathfinding.lua
@@ -1,3 +1,59 @@
-pathfinding = {}
+pathfinding = {
+    functions = {}
+}
+
+--TODO: REVISIT THIS LATER and make it work :(
+
+--function pathfinding.nodeCreate(position, distance, priority, possibleDirections, self)
+--    if self == nil then
+--        self = {}
+--    end
+--    self.position = position
+--    self.distance = distance
+--    self.priority = priority
+--    self.possibleDirections = possibleDirections
+--
+--    self.updatePriority = function (self, x, y)
+--        self.priority = self.distance + mathWorker.tileDistance(self.position.tileX - x, self.position.tileY - y)
+--    end
+--
+--    self.move = function (self, d)
+--        if self.possibleDirections == 8 and d % 2 ~= 0 then
+--            self.distance = self.distance + 14
+--        else
+--            self.distance = self.distance + 10
+--        end
+--    end
+--
+--    local metatable = {
+--        __lt = function (a, b)
+--            return a.priority < b.priority
+--        end
+--    }
+--
+--    setmetatable(self, metatable)
+--
+--    return self
+--end
+--
+--function pathfinding.functions.chooseDirection(x, possibleDirections)
+--    return (math.floor(x + possibleDirections / 2)) % possibleDirections
+--end
+--
+--function pathfinding.functions.genPath(dirMap, xA, yA, x, y, dx, dy, possibleDirections)
+--    dx = dx or {1,1,0,-1,-1,-1,0,1}
+--    dy = dy or {0,1,1,1,0,-1,-1,-1}
+--    possibleDirections = possibleDirections or 8
+--    path = {}
+--    while not (x == xA and y == yA) do
+--        local j = dirMap[y][x]
+--        local c = tostring(pathfinding.functions.chooseDirection(j, possibleDirections))
+--        table.insert(path, c)
+--        x = x + dx[j]
+--        y = y + dy[j]
+--    end
+--
+--    return path
+--end
 
 return pathfinding
--- source/workers/libs/mathWorker.lua
+++ source/workers/libs/mathWorker.lua
@@ -4,4 +4,12 @@ function mathWorker.lerp(a, b, t)
     return a + (b - a) * t
 end
 
+function mathWorker.tileDistance(a, b)
+    return (((a.tileX - b.tileX) ^ 2) + ((a.tileY - b.tileY) ^ 2)) ^ 0.5
+end
+
+function mathWorker.positionDistance(a, b)
+    return (((a.x - b.x) ^ 2) + ((a.y - b.y) ^ 2)) ^ 0.5
+end
+
 return mathWorker
