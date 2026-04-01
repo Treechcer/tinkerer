a27fc876c8d846c3d318e75728cf51411c41be9f
treecher <treechcer@gmail.com>
2026-04-01 23:47:44 +0200

    added text wrapping for description :)

--- source/player/items/description.lua
+++ source/player/items/description.lua
@@ -1,6 +1,7 @@
 --this is for item descriptions, there is also "description" in the itemIndex or it can be auto generated in a way
 
 description = {
+    splitBy = 24,
     f = {}
 }
 ---@param item string
@@ -14,13 +15,31 @@ function description.f.gen(item, bonus)
     str = str .. "Item: " .. item:gsub("_"," ")
 
     if itemIndex[item].typeI ~= nil then
-       str = str .. "\n" .. "Type: " .. itemIndex[item].typeI
+       str = str .. "\n" .. description.f.splitBy("Type: " .. itemIndex[item].typeI, description.splitBy)
     end
 
     if itemIndex[item].descriptor ~= nil then
         if itemIndex[item].descriptor ~= "" then
-            str = str .. "\n" .. "description: " .. itemIndex[item].descriptor
+            str = str .. "\n" .. description.f.splitBy("description: " .. itemIndex[item].descriptor, description.splitBy)
         end
+    end
+
+    return str
+end
+
+function description.f.splitBy(str, num)
+    local unchangednum = num
+    if str:len() > num then
+        for i = -5, 5, 1 do
+            local res = str:sub(num + i, num + i)
+            print(res)
+            if res == " " then
+                num = num + i
+                break
+            end
+        end
+        local nextStr = str:sub(1, num) .. "\n" .. description.f.splitBy(str:sub(num+1, str:len()), unchangednum)
+        return nextStr
     end
 
     return str
